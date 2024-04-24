e7a8cf8dc24e25b165cdeb088e9a26259988218b
Danial Harith <danialharith@infopro.com.my>
2024-04-24 16:34:21 +0800

    header update

--- project2/header.php
+++ project2/header.php
@@ -1,37 +1,56 @@
-<link rel="stylesheet" href="css/header.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <header>
     <div class="title">
-        <a href="index.php">UEVENT.HUB</a>
+        <a href="index.php">
+            <img src="" alt="">
+        </a>
     </div>
     <div class="d-flex">
         <nav>
+            <div class="top-nav">
+                <div class="lang">
+                    <button class="btn-lang-bm btn-lang">BM</button>
+                    |
+                    <button class="btn-lang-eng btn-lang">ENG</button>
+                </div>
+                <div class="location">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <div class="location-country-name">Malaysia</div>
+                </div>
+            </div>
             <ul>
-                <li><a href="index.php">Home</a></li>
-                <?php 
-                    if ($_SESSION['user_type'] == 'user') {
+                <?php
+                // if ($_SESSION['user_type'] == 'user') {
                 ?>
-                <li><a href="activity.php">Activity</a></li>
-                <?php } ?>
-                <li><a href="profile.php">Profile</a></li>
+                <!-- <li><a href="activity.php">Activity</a></li> -->
+                <?php
+                // } 
+                ?>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Admissions</a></li>
+                <li><a href="">Event</a></li>
+                <li><a href="">Career Opportunity</a></li>
+                <button class="btn-search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
             </ul>
         </nav>
-        <div class="searchbar-container">
-            <input list="events" name="events" placeholder="Search">
-            <datalist id="events">
-                <?php
-                    include_once('config.php');
-                    $result = mysqli_query($conn, "SELECT * FROM events WHERE status=1 & approved=1"); 
-                    while($row = mysqli_fetch_assoc($result)) {
-                ?>
-                <option value="<?= $row['event_name'] ?>">
-                <?php } ?>
-            </datalist>
-        </div>
-        <a href="signout.php" class="signout">
+        <!-- <div class="searchbar-container"> -->
+        <!-- <input list="events" name="events" placeholder="Search"> -->
+        <!-- <datalist id="events"> -->
+        <?php
+        include_once('config.php');
+        // $result = mysqli_query($conn, "SELECT * FROM events WHERE status=1 & approved=1"); 
+        // while($row = mysqli_fetch_assoc($result)) {
+        ?>
+        <!-- <option value="<?= $row['event_name'] ?>"> -->
+        <?php //} 
+        ?>
+        <!-- </datalist> -->
+        <!-- </div> -->
+        <!-- <a href="signout.php" class="signout">
             <span class="material-symbols-outlined">
                 logout
             </span>
-        </a>
+        </a> -->
     </div>
 </header>
