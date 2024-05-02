6f660d1272e3d62474c13bd7f1b57667ec00aceb
Danial Harith <danialharith@infopro.com.my>
2024-05-02 11:59:22 +0800

    init_user update

--- project2/init_user.php
+++ project2/init_user.php
@@ -1,31 +1,44 @@
 <?php
     include_once('config.php');
 
-    // Create new user/admin/organizer
-    // $username = "admin";
-    // $email = "admin1@mail.com";
-    // $password = "admin123";
-    // $user_type = "admin";
-    $username = "user1";
-    $email = "user1@mail.com";
-    $password = "123456";
-    $user_type = "user";
-    // exit();  
+    // Check if at least one row of user exist
+    $resultCheck = mysqli_query($conn, 'SELECT COUNT(user_id) AS row_count FROM users');
+    if ($resultCheck) {
+        $row = mysqli_fetch_assoc($resultCheck);
+        if ($row['row_count'] > 0) {
+            // echo "A user exist.";
+        } else {
+            // echo "users table is empty. <br>";
 
-    $password = password_hash($password, PASSWORD_DEFAULT);
+            // Create new user/admin/organizer
+            // $username = "admin";
+            // $email = "admin1@mail.com";
+            // $password = "admin123";
+            // $user_type = "admin";
+            $username = "User101";
+            $email = "user1@gmail.com";
+            $password = "user123";
+            $user_type = "user";
 
-    $query = "INSERT INTO users (username, email, password, user_type)
-                VALUES ('$username', '$email', '$password', '$user_type')";
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $prefix = date('Ym-');
+            $code = sprintf("%'.05d", 1);
+            $user_code = $prefix . $code;
 
-    try {
-        $result = mysqli_query($conn, $query);
-        if ($result) {
-            echo "User $username created successfully";
+            $query = "INSERT INTO users (user_code, username, email, password, user_type)
+                    VALUES ('$user_code', '$username', '$email', '$password', '$user_type')";
+
+            try {
+                $result = mysqli_query($conn, $query);
+                if ($result) {
+                    // echo "User <b>$username</b> created successfully.";
+                }
+            } catch (Exception $e) {
+                if (mysqli_errno($conn) == 1062) {
+                    // echo "A user with the same email already exist.";
+                }
+            }
         }
-    } catch (Exception $e) {
-        if (mysqli_errno($conn) == 1062) {
-            echo "Duplicate entry.";
-            // echo $e;
-        }
+        mysqli_free_result($resultCheck);
     }
 ?>
